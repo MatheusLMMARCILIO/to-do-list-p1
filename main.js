d043f74a4f231cdeb28f55542871ac2b384c81d6
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2026-02-06 19:12:45 -0300

    ok

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const changeBtn = document.querySelector(".changrBtn");
-const formChange = document.querySelector(".formChange");
-const newAsk = document.querySelector(".newAsk");
 
-changeBtn.addEventListener("click", (e) => {
-  formChange.style.display = "flex";
-  changeBtn.style.display = "none";
-});
-
-newAsk.addEventListener("click", () => {
-
-});
