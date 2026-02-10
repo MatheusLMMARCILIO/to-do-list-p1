be86da9cee1ac900b0d975832437f528c7577d4d
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2026-02-10 19:21:28 -0300

    olk

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const input = document.querySelector(".inputValueText");
+const btnNewAsk = document.querySelector(".newAsk");
 
+
+
+btnNewAsk.addEventListener("click", (e) => {
+  e.preventDefault();
+console.log(input.value)
+});
